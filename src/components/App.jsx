24ff8198e08bd0f4f35de81f48986d7bbdba8c74
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-05-06 19:37:54 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
-import { RecipeList } from './RecipeList';
-import recipes from '../recipes.json';
+import { Couner } from './Counter';
+import { SignupForm } from './SignUpForm';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#fc0303' },
+  { label: 'white', color: '#FFFFFF' },
+  { label: 'green', color: '#00ff11' },
+  { label: 'blue', color: '#5cabff' },
+  { label: 'orange', color: '#ff9736' },
+  { label: 'violet', color: '#bf36ff' },
+];
 
 export const App = () => {
   return (
     <div>
-      <RecipeList items={recipes} />
+      <SignupForm />
+      <Couner />
     </div>
   );
 };
